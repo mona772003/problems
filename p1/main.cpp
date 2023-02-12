cb98b026f9d3b17333f1a1e1e91ab23f6a217e4a
mona772003 <118972888+mona772003@users.noreply.github.com>
2023-02-12 17:34:51 +0200

    Add files via upload

---
+++ p1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello USER ENTER NUM!" << endl;
+    int x;
+    cin>> x;
+
+    if ( x /1==x ||x/x==1 ){
+
+
+
+
+
+    cout << "prime!" << endl;
+    }
+    else
+            cout << " not prime!" << endl;
+
+    return 0;
+}
