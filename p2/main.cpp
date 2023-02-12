bbf174cf79c0a61e26f124848f3452fccd3f6959
mona772003 <118972888+mona772003@users.noreply.github.com>
2023-02-12 17:39:56 +0200

    Add files via upload

---
+++ p2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+for ( int x ; x<=1000 ;x++)
+{
+    if ( x%3!=0 && x%4!=0 && x%6!=0)
+        cout << x<<endl;
+}
+
+
+
+    return 0;
+}
