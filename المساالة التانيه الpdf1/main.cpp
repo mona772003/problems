3d1bfdcbcb00d595148d9b7643eb7816c7ee30d9
mona772003 <118972888+mona772003@users.noreply.github.com>
2023-02-12 17:54:59 +0200

    Add files via upload

---
+++ المساالة التانيه الpdf1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cin >>a>>b>>c>>D;
+    if ((a+B+C)==d){
+        cout << "yes!" << endl;
+    }
+
+
+    else if ((a-B-C)==d){
+        cout << "yes!" << endl;
+    }
+
+
+    else if ((a*B*C)==d){
+        cout << "yes!" << endl;
+        }
+            else {
+                cout << "NO!" << endl;
+            }
+
+
+
+
+
+
+
+    return 0;
+}
